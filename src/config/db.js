cc4b0a839124dd965523dbff8868b4daa335d7e6
Lele <leticia_nasciment155@estudante.sesisenai.org.br>
2026-09-21 17:10:36 -0300

    commit 6

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
 import { Pool } from "pg"
 import "dotenv/config"
 export const pool = new Pool ()
-=======
-import express from "express";
-import celularRouter from "../src/routes/celular.routes.js"
-
-const app = express()
-const port = 3000
-
-app.use(express.json())
-
-app.use("/celulares", celularRouter)
-
-app.listen(port, () => {
-    console.log(`App rodando em http://localhost:3000`);
-})
->>>>>>> fa896309940a616057cc809312847d76eb562e55
