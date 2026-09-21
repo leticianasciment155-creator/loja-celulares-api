fa896309940a616057cc809312847d76eb562e55
Lele <leticia_nasciment155@estudante.sesisenai.org.br>
2026-09-21 16:50:38 -0300

    commit 3

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,13 @@
 import express from "express";
-import celularRoutes from "../src/routes/celular.routes.js"
+import celularRouter from "../src/routes/celular.routes.js"
 
+const app = express()
+const port = 3000
 
-app.
+app.use(express.json())
+
+app.use("/celulares", celularRouter)
+
+app.listen(port, () => {
+    console.log(`App rodando em http://localhost:3000`);
+})
