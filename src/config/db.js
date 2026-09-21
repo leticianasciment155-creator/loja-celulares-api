1b77914213e2399bf48644c1c0bf1d43f6f9d714
Lele <leticia_nasciment155@estudante.sesisenai.org.br>
2026-09-21 16:36:58 -0300

    commit 2

---
+++ src/config/db.js
@@ -0,0 +1,5 @@
+import express from "express";
+import celularRoutes from "../src/routes/celular.routes.js"
+
+
+app.
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import { Pool } from "pg"
+import "dotenv/config"
+export const Pool = new Pool ()
